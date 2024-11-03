31b95a0f6ed61c0dacba6682d4b9e72d33d40b1e
Andy Cuong <nguyenduccuong11102003@gmail.com>
2024-11-03 22:08:13 +0200

    Optimize imports

--- app/src/main/java/com/example/flightsearch/MainActivity.kt
+++ app/src/main/java/com/example/flightsearch/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.flightsearch.ui.SearchScreen
 import com.example.flightsearch.ui.theme.FlightSearchTheme
